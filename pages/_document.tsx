6c579c4d84ee6f9dfd3455d889c882fd39100478
Dodokhov Alim <dodokhov.alim@gmail.com>
2023-01-24 10:08:15 +0300

    CU-338ybkm — dzen add

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -24,6 +25,23 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <Script
+          id="yandex"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(92161912, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });`,
+          }}
+        />
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/92161912"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </Html>
   );
