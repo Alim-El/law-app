34d21b7311d996500f53d8f0b05295d627bfd7d4
Dodokhov Alim <dodokhov.alim@gmail.com>
2023-01-24 10:20:38 +0300

    CU-338ybkm — dzen add

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,16 +21,17 @@ export default function Document() {
           name="google-site-verification"
           content="qo7hCt_pOJ4faLdzBtrjxpB5tefBFyCGLstOYwvA_nE"
         />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
 
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(92161912, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });`,
           }}
         />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+
         <noscript>
           <div>
             {/* eslint-disable-next-line @next/next/no-img-element */}
